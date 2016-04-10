3fed82590571402a9dda8e4589208fa30c45c89a
RiRomain <romain.rinie@xcom.de>
2016-04-10 22:38:09 +0200

    Push button support
    Add push button support to turn led on/off on single press and enter AP
    config mode after long press (10 secondes)
    
    First code to support DS18B20 one wire sensor (only declarative to
    investigate library conflict, usage not yet implemented)

--- OakRGBDriver.ino
+++ OakRGBDriver.ino
@@ -2,15 +2,31 @@
  OAK RGB Controller by Rinié Romain
  Control an RGB PWM driver from a Digistump OAK board.
 */
+//#define TEMP_SENSOR //Uncomment/comment out to activate/deactivate the temperature sensor
+
+#if defined TEMP_SENSOR
+  #include <OneWire.h>
+#endif
 
 int ledPin = 1;       // choose the pin for the status LED
-//int inPin = 9;        // choose the input pin (for a pushbutton)
-//int pinVal = 0;       // variable for reading the pin status
+int pushButtonInputPin = 10;        // choose the input pin (for a pushbutton)
+int pushButtonValue = 0;       // variable for reading the pin status
+int lastPushButtonValue = 0;   // variable for keeping previous button value
+
+#if defined TEMP_SENSOR
+  int DS18S20_Pin = 2;    //choose the pin for the DS18B20 PIN
+#endif
 
 int redLedPin = 6;    //choose the pin for the RED LED
 int greenLedPin = 7;  //choose the pin for the GREEN LED
 int blueLedPin = 8;   //choose the pin for the BLUE LED
 int whiteLedPin = 9;  //choose the pin for the WHITE LED
+
+#if defined TEMP_SENSOR
+  float tempC = 0;     // Temperature in degrees C
+  char tempCstring[6]; // Used to store a temperature value as a string
+  OneWire ds(DS18S20_Pin);
+#endif
 
 int redValue = 0;
 int greenValue = 0;
@@ -18,6 +34,8 @@ int blueValue = 0;
 int whiteValue = 0;
 int intensity = 0;
 int maxIntensity = 1023;
+
+boolean ledAreOn = false;
 
 int getValueWithAppliedIntensity(int baseValue) {
   if (0 == baseValue) {
@@ -48,10 +66,12 @@ void turnAllLedOff() {
 int ledToggleFunction(String command) {
   if (command=="on") {
         setPresetValueToOutput();
+        ledAreOn = true;
         return 1;
     }
     else if (command=="off") {
         turnAllLedOff();
+        ledAreOn = false;
         return 1;
     }
     else {
@@ -90,8 +110,8 @@ void setup() {
   pinMode(greenLedPin, OUTPUT);   // Initialize the RGB_GREEN_LED pin as an output
   pinMode(blueLedPin, OUTPUT);    // Initialize the RGB_BLUE_LED pin as an output
   pinMode(whiteLedPin, OUTPUT);   // Initialize the RGB_WHITE_LED pin as an output
+  pinMode(pushButtonInputPin, INPUT);          //Initialize the input push button
   
-  //pinMode(inPin, INPUT);          //Initialize the input push button
   Particle.function("led", ledToggleFunction);
   Particle.function("value", setValue);
   Particle.function("intensity", setIntensity);
@@ -100,11 +120,44 @@ void setup() {
   Particle.variable("blue", blueValue);
   Particle.variable("white", whiteValue);
   Particle.variable("inten", intensity);
+//  char bla[] = "test";
+//  Particle.variable("test", bla);
   setPresetValueToOutput();
 }
 
+//Time storage is preffered over pause function to create a delay, avoiding by this way to slow down the rest of the execution
+unsigned long previousLoopMillis = 0;     // Storage of the last time the push button value was read
+const long buttonReadingInterval = 100; // Delay between 2 push button read value, button will have to be pressed at most 100mS to react
+const long numberOfLoopBeforeWifiSetup = 100; //represent 10 secondes
+unsigned long pushLoopCount = 0;
+
 // the loop function runs over and over again forever
 void loop() {
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousLoopMillis >= buttonReadingInterval) {
+    // save the last time we read the push button value
+    previousLoopMillis = currentMillis;  
+    pushButtonValue = digitalRead(pushButtonInputPin);
+    if (pushButtonValue != lastPushButtonValue) {
+       if (pushButtonValue == LOW) { //Button pressed
+            if (ledAreOn) {
+              ledToggleFunction("off");
+            } else {
+              ledToggleFunction("on");
+            }
+        }
+        lastPushButtonValue = pushButtonValue;
+        pushLoopCount = 0; //Register the first pushed loop
+    } else { //Value not changed
+      if (pushButtonValue == LOW) { //Button still pressed
+        pushLoopCount = pushLoopCount + 1;
+      }
+    }
+    if (pushLoopCount > numberOfLoopBeforeWifiSetup) {
+      Oak.rebootToConfig(); 
+      pushLoopCount = 0;
+    }
+  }
 }
 
 
