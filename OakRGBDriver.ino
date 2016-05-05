f8275de9c54c40fe5ef065d62ab5620879fa713b
RiRomain <romain.rinie@xcom.de>
2016-05-05 11:02:10 +0200

    Comment beautify + DS18B20 suppression
    Addition of comment on the code.
    Deletion of the first DS18B20 code, the DS18B20 will  be handled in a
    separate project.

--- OakRGBDriver.ino
+++ OakRGBDriver.ino
@@ -2,41 +2,39 @@
  OAK RGB Controller by Rinié Romain
  Control an RGB PWM driver from a Digistump OAK board.
 */
-//#define TEMP_SENSOR //Uncomment/comment out to activate/deactivate the temperature sensor
 
-#if defined TEMP_SENSOR
-  #include <OneWire.h>
-#endif
+/*****************************************************************
+ USER SETTINGS: You can redefine the different output in this part.
+ *****************************************************************/
 
-int ledPin = 1;       // choose the pin for the status LED
-int pushButtonInputPin = 10;        // choose the input pin (for a pushbutton)
-int pushButtonValue = 0;       // variable for reading the pin status
-int lastPushButtonValue = 0;   // variable for keeping previous button value
+int ledPin = 1;       // choose the pin for the status LED (Oak on-board LED)
+int pushButtonInputPin = 10;   // choose the input pin (for a pushbutton)
 
-#if defined TEMP_SENSOR
-  int DS18S20_Pin = 2;    //choose the pin for the DS18B20 PIN
-#endif
+int redLedPin = 6;    //choose the pin for the RED LED channel
+int greenLedPin = 7;  //choose the pin for the GREEN LED channel
+int blueLedPin = 8;   //choose the pin for the BLUE LED channel
+int whiteLedPin = 9;  //choose the pin for the WHITE LED channel
 
-int redLedPin = 6;    //choose the pin for the RED LED
-int greenLedPin = 7;  //choose the pin for the GREEN LED
-int blueLedPin = 8;   //choose the pin for the BLUE LED
-int whiteLedPin = 9;  //choose the pin for the WHITE LED
 
-#if defined TEMP_SENSOR
-  float tempC = 0;     // Temperature in degrees C
-  char tempCstring[6]; // Used to store a temperature value as a string
-  OneWire ds(DS18S20_Pin);
-#endif
-
+/*****************************************************************
+ PROGRAMM VARIABLE: Do not modified following value.
+ *****************************************************************/
 int redValue = 0;
 int greenValue = 0;
 int blueValue = 0;
 int whiteValue = 0;
 int intensity = 0;
-int maxIntensity = 1023;
+int maxIntensity = 1023;  //Max output for the PWM, 
 
+int pushButtonValue = 0;       // variable for the actual push button status
+int lastPushButtonValue = 0;   // variable for the previous push button value
 boolean ledAreOn = false;
 
+/*
+ * Output value calculation
+ * @Return: baseValue with applied intensity and maxIntensity
+ * @Param baseValue: value between 0 and 255
+ */
 int getValueWithAppliedIntensity(int baseValue) {
   if (0 == baseValue) {
     return 0;
@@ -47,6 +45,9 @@ int getValueWithAppliedIntensity(int baseValue) {
   return (((baseValue*intensity)/255)*maxIntensity)/255;
 }
 
+/*
+ * Turn on all the led to the actual set value
+ */
 void setPresetValueToOutput() {
   analogWrite(ledPin, getValueWithAppliedIntensity(intensity));
   analogWrite(redLedPin, getValueWithAppliedIntensity(redValue));
@@ -55,6 +56,9 @@ void setPresetValueToOutput() {
   analogWrite(whiteLedPin, getValueWithAppliedIntensity(whiteValue));
 }
 
+/*
+ * Turn off all the led
+ */
 void turnAllLedOff() {
   analogWrite(ledPin, 0);
   analogWrite(redLedPin, 0);
@@ -63,6 +67,13 @@ void turnAllLedOff() {
   analogWrite(whiteLedPin, 0);
 }
 
+/*
+ * LED ON/OFF Particle registered function
+ * @Return: return 1 if command was executed, -1 if command is not supported
+ * @Param command: on or off. 
+ * on:  turn all led to the preset value
+ * off: turn all led off
+ */
 int ledToggleFunction(String command) {
   if (command=="on") {
         setPresetValueToOutput();
@@ -79,10 +90,23 @@ int ledToggleFunction(String command) {
     }
 }
 
+/*
+ * Extraction from int from a string
+ * @Return: Extracted int value
+ * @Param startIndex: start index of the integer to extract
+ * @Param endIndex:   end   index of the integer to extract
+ * @Param commandIn:  string to extract the integer from
+ */
 int stringToInt(int startIndex, int endIndex, String commandIn) {
   return commandIn.substring(startIndex, endIndex).toInt();
 }
 
+/*
+ * LED Intensity Particle registered function
+ * @Return: return 1 if command was registered, -1 if command is not supported
+ * @Param command: intensity to set, from 000 to 255. 
+ * 0 or 00 won't be accepted, value should be 3 decimal, like 000 or 014 and not 0 or 14.
+ */
 int setIntensity(String command) {
   if (command.length() != 3) {
     return -1;  
@@ -92,6 +116,12 @@ int setIntensity(String command) {
   return 1;
 }
 
+/*
+ * LED value Particle registered function
+ * @Return: return 1 if command was registered, -1 if command is not supported
+ * @Param command: value to set, from 000000000000 to 255255255255 in the sequence RGBW. 
+ * 0 or 00 won't be accepted, value should be 3 decimal, like 000 or 014 and not 0 or 14.
+ */
 int setValue(String command) {
   if (command.length() != 12) {
     return -1;
@@ -112,23 +142,21 @@ void setup() {
   pinMode(whiteLedPin, OUTPUT);   // Initialize the RGB_WHITE_LED pin as an output
   pinMode(pushButtonInputPin, INPUT);          //Initialize the input push button
   
-  Particle.function("led", ledToggleFunction);
-  Particle.function("value", setValue);
-  Particle.function("intensity", setIntensity);
-  Particle.variable("red", redValue);
-  Particle.variable("green", greenValue);
-  Particle.variable("blue", blueValue);
-  Particle.variable("white", whiteValue);
-  Particle.variable("inten", intensity);
-//  char bla[] = "test";
-//  Particle.variable("test", bla);
+  Particle.function("led", ledToggleFunction);  // "led" function to turn the led on or off
+  Particle.function("value", setValue);         // "value" function to set the led value
+  Particle.function("intensity", setIntensity); // "intensity" function to set the led intensity
+  Particle.variable("red", redValue);           // "red"   variable representing the red   led value (on 255)
+  Particle.variable("green", greenValue);       // "green" variable representing the green led value (on 255)
+  Particle.variable("blue", blueValue);         // "blue"  variable representing the blue  led value (on 255)
+  Particle.variable("white", whiteValue);       // "white" variable representing the white led value (on 255)
+  Particle.variable("inten", intensity);        // "inten" variable representing the led intensity (on 255)
   setPresetValueToOutput();
 }
 
 //Time storage is preffered over pause function to create a delay, avoiding by this way to slow down the rest of the execution
 unsigned long previousLoopMillis = 0;     // Storage of the last time the push button value was read
 const long buttonReadingInterval = 100; // Delay between 2 push button read value, button will have to be pressed at most 100mS to react
-const long numberOfLoopBeforeWifiSetup = 100; //represent 10 secondes
+const long numberOfLoopBeforeWifiSetup = 100; //represent 10 secondes, after which the Oak will go in wifi config mode
 unsigned long pushLoopCount = 0;
 
 // the loop function runs over and over again forever
